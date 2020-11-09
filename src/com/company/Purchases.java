0c1e62ba6907ad8d51074b2a999a22deeddbec40
Tamia Powell <tamiapowell@Tamias-MacBook-Pro-2.local>
2020-11-09 15:44:49 -0600

    Initial commit

---
+++ src/com/company/Purchases.java
@@ -0,0 +1,23 @@
+package com.company;
+
+public class Purchases {
+
+    private String purchItem;
+    private double purchPrice;
+
+    public String getPurchItem() {
+        return purchItem;
+    }
+
+    public void setPurchItem(String purchItem) {
+        this.purchItem = purchItem;
+    }
+
+    public double getPurchPrice() {
+        return purchPrice;
+    }
+
+    public void setPurchPrice(double purchPrice) {
+        this.purchPrice = purchPrice;
+    }
+}
